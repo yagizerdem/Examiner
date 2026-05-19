3af819e2adaa0b5b0412b09a8624cc4145abb893
yagiz erdem <2yagiz004@gmail.com>
2026-05-20 01:34:08 +0300

    async handler util added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const {router : authRouter} = require("./routes/auth-router")
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/auth", authRouter);
 
 
 
